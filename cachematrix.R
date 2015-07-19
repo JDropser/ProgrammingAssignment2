da099b5df65a4ad901449b5505407caec852f640
Kathy Froehlich <kkf1962@gmail.com>
2015-07-19 13:11:02 -0500

    Data Science - R Programming
    Programming Assignment 2:  Optimization for Matrix Inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## The object is really a list of functions that:
+## Set the matrix to be inverted
+## Get the matrix to be inverted
+## Get the inverted matrix
+## Set the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverted matrix is already available from cache.  
+## If not, the function gets the matrix to be inverted, inverts the matrix and caches the inverted matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
